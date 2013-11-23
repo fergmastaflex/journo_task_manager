0bd4fdf1815e58b5700065b1cc4238b93c0f08bc
ferg <joey@fergmedia.com>
2013-11-23 10:22:23 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournoTaskManager::Application.config.secret_token = '78d2ee851e694442a0bf60767f51fb67d4073f4415586cbdd12fbfe1b2c92b90b5ce4ae536878db9fc1c10562c87cb8eb10e0cc00a1ac54fc70338190954c4a1'
